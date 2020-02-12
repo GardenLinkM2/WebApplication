174a39a9606e53886aaa723e40e0ee59f55d88dd
Hakan M2 <master_hakan@local.dev>
2020-02-12 14:20:44 +0000

    Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ScreensModule} from './screens/screens.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScreensModule
   ],
   providers: [],
   bootstrap: [AppComponent]
