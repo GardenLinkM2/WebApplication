c9fffc816c3e79c7a54b23b0251564d81425d242
Eric <master_eric@local.dev>
2020-02-15 13:05:21 +0100

    component name refactoring

--- src/app/screens/mdp-oublie/mdp-oublie.component.spec.ts
+++ src/app/screens/mdp-oublie/mdp-oublie.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MpdOublieComponent } from './mpd-oublie.component';
+import { MdpOublieComponent } from './mdp-oublie.component';
 
 describe('MpdOublieComponent', () => {
-  let component: MpdOublieComponent;
-  let fixture: ComponentFixture<MpdOublieComponent>;
+  let component: MdpOublieComponent;
+  let fixture: ComponentFixture<MdpOublieComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MpdOublieComponent ]
+      declarations: [ MdpOublieComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MpdOublieComponent);
+    fixture = TestBed.createComponent(MdpOublieComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
