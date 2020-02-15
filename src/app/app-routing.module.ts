096fd0de56d8e58ef56c0002328b63719438e2fa
Eric <master_eric@local.dev>
2020-02-15 13:02:56 +0100

    added route to Forgotten password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeScreenComponent} from './screens/home-screen/home-screen.component';
-
+import {MdpOublieComponent} from './screens/mdp-oublie/mdp-oublie.component';
 
 const routes: Routes = [
   { path: 'accueil', component: HomeScreenComponent },
+  { path: 'mdpoublie', component: MdpOublieComponent},
   { path: '**',   redirectTo: '/accueil', pathMatch: 'full'}
 ];
 
