fd1c7f9d140f94a2829b0ec95fc184d3b8cc62ba
Eric <master_eric@local.dev>
2020-02-25 21:05:31 +0100

    Fixed test for services creation

--- src/app/screens/recover-pwd-screen/recover-pwd.service.spec.ts
+++ src/app/screens/recover-pwd-screen/recover-pwd.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RecoverPwdService } from './recover-pwd.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('RecoverPwdService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule, RouterTestingModule],
+    providers: [RecoverPwdService]
+  }));
 
   it('should be created', () => {
     const service: RecoverPwdService = TestBed.get(RecoverPwdService);
