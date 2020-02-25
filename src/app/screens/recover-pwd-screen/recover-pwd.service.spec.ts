e327b5c986b67ea923b32fd19cd9b4aa07b259f0
Eric <master_eric@local.dev>
2020-02-25 21:55:16 +0100

    Added some tests

--- src/app/screens/recover-pwd-screen/recover-pwd.service.spec.ts
+++ src/app/screens/recover-pwd-screen/recover-pwd.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { RecoverPwdService } from './recover-pwd.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 describe('RecoverPwdService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -13,5 +14,13 @@ describe('RecoverPwdService', () => {
   it('should be created', () => {
     const service: RecoverPwdService = TestBed.get(RecoverPwdService);
     expect(service).toBeTruthy();
+  });
+
+  it('#reinint works', () => {
+    const service: RecoverPwdService = TestBed.get(RecoverPwdService);
+    const userForm = new FormGroup(
+      {password: new FormControl('', [Validators.required])}
+    );
+    expect(service.reinitpass.bind(userForm)).toThrowError(TypeError);
   });
 });
