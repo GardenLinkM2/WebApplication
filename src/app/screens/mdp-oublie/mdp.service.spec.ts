00e4ceae2fd188014bb5744cf68f234398ebe1dc
Eric <master_eric@local.dev>
2020-02-25 21:55:16 +0100

    Added some tests

--- src/app/screens/mdp-oublie/mdp.service.spec.ts
+++ src/app/screens/mdp-oublie/mdp.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { MdpService } from './mdp.service';
 import { HttpClientTestingModule} from '@angular/common/http/testing';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 describe('MdpService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -12,5 +13,12 @@ describe('MdpService', () => {
   it('should be created', () => {
     const service: MdpService = TestBed.get(MdpService);
     expect(service).toBeTruthy();
+  });
+  it('#recover works', () => {
+    const service: MdpService = TestBed.get(MdpService);
+    const userForm = new FormGroup(
+      {falsified: new FormControl('', [Validators.email, Validators.required])}
+    );
+    expect(service.recover.bind(userForm)).toThrowError(TypeError);
   });
 });
