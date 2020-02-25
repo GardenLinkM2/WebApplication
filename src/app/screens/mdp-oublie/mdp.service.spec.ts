e7de7442e41cb5dce4107e258bf8f27f498ba677
Eric <master_eric@local.dev>
2020-02-25 21:05:31 +0100

    Fixed test for services creation

--- src/app/screens/mdp-oublie/mdp.service.spec.ts
+++ src/app/screens/mdp-oublie/mdp.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MdpService } from './mdp.service';
+import { HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('MdpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [MdpService]
+  }));
 
   it('should be created', () => {
     const service: MdpService = TestBed.get(MdpService);
