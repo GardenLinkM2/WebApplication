e6f7ae319a087e58805b1bb2953d7f7beb8a46f1
Eric <master_eric@local.dev>
2020-03-01 18:56:03 +0100

    changed token

--- src/app/features/user-information/user.service.ts
+++ src/app/features/user-information/user.service.ts
@@ -18,10 +18,10 @@ export class UserService {
   }
   getUserInfoAuth(id: string) {
     this.req = new HttpRequest('GET', this.urlauth + `users/${id}`,
-      { headers: new HttpHeaders({Authorization: 'eyJraWQiOiJhY2NvdW50IiwiYWxnIjoiSFM1MTIifQ.eyJzdWIiOiJyaWNreWJs' +
-            'YWNrd2FsbEBnbWFpbC5jb20iLCJraWQiOiJhY2NvdW50IiwiaXNBZG1pbiI6ZmFsc2UsImV4cCI6MTU4NTA2NTI1OSwidXVpZCI6IjkwYTVmY2NjL' +
-            'TVkZTktNGQyZS1iMjYwLTNjYjkwY2I4M2E5YSIsImVtaXR0ZXIiOiJhY2NvdW50IiwianRpIjoiNGZjNjE2YTUtOGU2Ni00NTNlLTg3ZDEtODQzOG' +
-            'I2NTBhMTZkIn0.qwtdLojoNcoA41OKzGL76AvDttwcqoAWd-PIglZw_Q7HR07KihchBM8Au4TRvbImwMd3pPhr0CibHwnT7PWuEQ'})});
+      { headers: new HttpHeaders({Authorization: 'eyJraWQiOiJhY2NvdW50IiwiYWxnIjoiSFM1MTIifQ.eyJzdWIiOiJyaWNreWJsYWNrd2' +
+            'FsbEBnbWFpbC5jb20iLCJraWQiOiJhY2NvdW50IiwiaXNBZG1pbiI6ZmFsc2UsImV4cCI6MTU4NTEyMzI2MiwidXVpZCI6IjkwYTVmY2NjLTVkZTktNGQyZS' +
+            '1iMjYwLTNjYjkwY2I4M2E5YSIsImVtaXR0ZXIiOiJhY2NvdW50IiwianRpIjoiZmNmMDBjOTktOTMyOC00MmU3LTg4NWUtODQ4NjRhZGUwN2Q4In0.UegapPz' +
+            '_NQyWl0cvwITXCMB5tAbavm2Dxu-_6zS07VcybDPuy36aJDDhJ-4vNMKHwgYhFf7vwYf_4zO5h29Vwg'})});
     return this.next.handle(this.req);
   }
 
