4c974811b58c6f832c6da924e4b31aaf488083a2
Eric <master_eric@local.dev>
2020-03-07 16:24:11 +0100

    nothing to report

--- src/app/req-interceptor.ts
+++ src/app/req-interceptor.ts
@@ -4,13 +4,13 @@ import {Observable} from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  private synToken = 'eyJhbGciOiJIUzUxMiIsImtpZCI6ImJhY2sifQ.eyJ1dWlkIjoiOTBhNWZjY2MtNWRlOS00ZDJlLWIyNjAtM2NiOTBjYjgzYTlhIiwic3Vi' +
-    'Ijoicmlja3libGFja3dhbGxAZ21haWwuY29tIiwiaXNBZG1pbiI6dHJ1ZSwiZXhwIjoxNTgzMzQzMjM2LjkxOTUyMzd9.RQmXucKmzCtxt8wTITSzg8K4kG_zuyi' +
-    'VOgzrcEKdJMiQ9HmqaTpD70MoyN-dZYJmunieMFNyzvRcgR-1YzT7iA';
-  private accessToken = 'eyJraWQiOiJhY2NvdW50IiwiYWxnIjoiSFM1MTIifQ.eyJzdWIiOiJyaWNreWJsYWNrd2' +
-    'FsbEBnbWFpbC5jb20iLCJraWQiOiJhY2NvdW50IiwiaXNBZG1pbiI6ZmFsc2UsImV4cCI6MTU4NTEyMzI2MiwidXVpZCI6IjkwYTVmY2NjLTVkZTktNGQyZS' +
-    '1iMjYwLTNjYjkwY2I4M2E5YSIsImVtaXR0ZXIiOiJhY2NvdW50IiwianRpIjoiZmNmMDBjOTktOTMyOC00MmU3LTg4NWUtODQ4NjRhZGUwN2Q4In0.UegapPz' +
-    '_NQyWl0cvwITXCMB5tAbavm2Dxu-_6zS07VcybDPuy36aJDDhJ-4vNMKHwgYhFf7vwYf_4zO5h29Vwg';
+  private synToken = 'eyJhbGciOiJIUzUxMiIsImtpZCI6ImJhY2sifQ.eyJ1dWlkIjoiODZhM2YxN2EtMWE3NC00MTAxLWFlZmItYjZkYWZkNmY5M2Q0Iiwic' +
+    '3ViIjoicmlja3libGFja3dhbGxAZ21haWwuY29tIiwiaXNBZG1pbiI6dHJ1ZSwiZXhwIjoxNTg0NDUyMjMxLjg0MjIwOTZ9.-UiQRVfqQJhOkQNM8ueYgtGqSU' +
+    'dN9YzsYWNo6uIbszsI2OoYXfIYeRKQ3ev9LUGx_JnSB572c4KOZbFngKqu1w';
+  private userToken = 'eyJraWQiOiJhY2NvdW50IiwiYWxnIjoiSFM1MTIifQ.eyJzdWIiOiJyaWNreWJsYWNrd2FsbEBnbWFpbC5jb20iLCJraWQiOiJhY2Nvd' +
+    'W50IiwiaXNBZG1pbiI6ZmFsc2UsImV4cCI6MTU4NTYzNjEzNSwidXVpZCI6Ijg2YTNmMTdhLTFhNzQtNDEwMS1hZWZiLWI2ZGFmZDZmOTNkNCIsImVtaXR0ZXIiOi' +
+    'JhY2NvdW50IiwianRpIjoiNzQ2OTViNzgtMTRhMC00NmJlLWJiY2MtMTMzYjczNTUzODM3In0.WOWVfy6oKx6ncyGeOEoo9cjY30XlSQs_rlSUmGM3ZlnxRGaj9ek' +
+    'vzob69L1I5SPzeA-sagA4ut3IxsZlPqjFXA';
   private modifiedReq: HttpRequest<any>;
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // NOTE: Replace the previous constant with your bearer for now.
