724ab2df37b2703a33b0c437238d673661cd9e9c
Eric <master_eric@local.dev>
2020-03-08 22:31:53 +0100

    component dynamism enhancement, account modification and deletion implemented

--- src/app/features/user-information/user-information.component.ts
+++ src/app/features/user-information/user-information.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from './user.service';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user-info/user.service';
+import {FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {ConfirmationService, Message} from 'primeng/api';
+
+interface UpdatedInfo {
+  'password': string;
+  'phone': string;
+  'email': string;
+  'avatar': string;
+  'newsletter': boolean;
+}
 
 @Component({
   selector: 'app-user-information',
@@ -9,72 +17,97 @@ import {ConfirmationService, Message} from 'primeng/api';
   styleUrls: ['./user-information.component.scss'],
   providers: [ConfirmationService]
 })
+
 export class UserInformationComponent implements OnInit {
   constructor(private userService: UserService, private confirmationService: ConfirmationService) { }
   private firstname: string;
   private lastname: string;
   private email: string;
   private balance: number;
+  private  id: string;
   private password = '****************';
   private passwordLabel: string;
   private phoneNumber: string;
   private msgs: Message[] = [];
   private  activateFields: boolean;
   private editingPassword: boolean;
+  private newInformation: UpdatedInfo;
+  private displaySuccessSuppression = false;
+  private displaySuccessModification: boolean;
   infoForm = new FormGroup({
-    firstname: new FormControl({value: this.firstname}, Validators.required),
-    lastname: new FormControl({value: this.lastname}, Validators.required),
     password: new FormControl({value: this.password}, Validators.required),
     email: new FormControl({value: this.email}, [Validators.email, Validators.required]),
-    phoneNumber: new FormControl({value: this.phoneNumber}, Validators.required)
+    phoneNumber: new FormControl({value: this.phoneNumber}, Validators.required),
+    confirmPassword: new FormControl({value: ''}, Validators.required)
   });
-  ngOnInit() {
+
+  comparisonValidator(): ValidatorFn {
+    return (group: FormGroup): ValidationErrors => {
+      const control1 = group.get('password');
+      const control2 = group.get('confirmPassword');
+      if (control1.value !== control2.value && control1.enabled && control2.value !== '') {
+        control2.setErrors({notEquivalent: 'Les deux saisies ne correspondent pas!'});
+      } else {
+        control2.setErrors(null);
+      }
+      return;
+    };
+  }
+
+  async ngOnInit() {
+    this.infoForm.setValidators(this.comparisonValidator());
+    this.displaySuccessModification = false;
     this.activateFields = false;
     this.editingPassword = false;
     this.passwordLabel = 'Mot de passe';
     this.infoForm.disable();
-    this.userService.getUserInfo().toPromise().then(
+    await this.userService.getUserWallet().toPromise().then(
       response => {
         // @ts-ignore
-        this.firstname = response.body.data.firstName;
-        this.infoForm.get('firstname').setValue(this.firstname);
+        this.balance = response.data.balance;
+      }
+    );
+    await this.userService.getUserInfoAuth().toPromise().then(
+      // @ts-ignore
+      responseAuth => {
         // @ts-ignore
-        this.lastname = response.body.data.lastName;
-        this.infoForm.get('lastname').setValue(this.lastname);
+        this.firstname = responseAuth.firstName;
         // @ts-ignore
-        this.email = response.body.data.email;
+        this.lastname = responseAuth.lastName;
+        // @ts-ignore
+        this.email = responseAuth.email;
         this.infoForm.get('email').setValue(this.email);
         // @ts-ignore
-        this.balance = response.body.data.wallet.balance;
+        this.phoneNumber = responseAuth.phone;
+        this.infoForm.get('phoneNumber').setValue(this.phoneNumber);
         // @ts-ignore
-        this.userService.getUserInfoAuth(response.body.data.id).toPromise().then(
-          // @ts-ignore
-          responseAuth => {this.phoneNumber = responseAuth.body.phone;
-                           this.infoForm.get('phoneNumber').setValue(this.phoneNumber); },
-          error => {console.log('ERROR', error); }
-        );
+        this.id = responseAuth.id;
+      },
+      error => {
+        console.log('ERROR', error);
       }
     );
   }
-
   confirmSuppression() {
     this.confirmationService.confirm({
       message: 'Etes vous sure de vouloir supprimer le compte?',
       header: 'Suppression',
       icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'You have accepted'}];
+      accept: async () => {
+        await this.userService.deleteAccount(this.id).subscribe(
+          () => this.displaySuccessSuppression = true,
+          error => console.log('Failure', error)
+        );
+        this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'Votre compte à été supprimé !!!'}];
       },
       reject: () => {
-        this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}];
+        this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'La suppression du compte à été annulée.'}];
       }
     });
   }
 
   switchFieldState() {
     this.activateFields = true;
-    this.infoForm.get('firstname').enable();
-    this.infoForm.get('lastname').enable();
     this.infoForm.get('email').enable();
     this.infoForm.get('phoneNumber').enable();
   }
@@ -87,7 +120,30 @@ export class UserInformationComponent implements OnInit {
       accept: () => {
         this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'You have accepted'}];
         this.activateFields = false;
-        this.infoForm.disable();
+        if (this.infoForm.get('password').disabled) {
+          this.newInformation = {
+            password: undefined,
+            phone: this.infoForm.get('phoneNumber').value,
+            email: this.infoForm.get('email').value,
+            avatar: 'urltoavatar',
+            newsletter: false,
+          };
+          delete this.newInformation.password;
+        } else {
+          this.newInformation = {
+            password: this.infoForm.get('password').value,
+            phone: this.infoForm.get('phoneNumber').value,
+            email: this.infoForm.get('email').value,
+            avatar: 'urltoavatar',
+            newsletter: false,
+          };
+          this.passwordLabel = 'Mot de passe';
+          this.editingPassword = false;
+        }
+        this.userService.updateInformation(this.id, this.newInformation).subscribe(
+          () => this.displaySuccessModification = true,
+          error => console.log('Failure', error)
+        );
       },
       reject: () => {
         this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}];
@@ -96,21 +152,21 @@ export class UserInformationComponent implements OnInit {
   }
 
   cancelAction() {
-    this.activateFields = false;
-    this.infoForm.disable();
-    this.editingPassword = false;
-    this.passwordLabel = 'Mot de passe';
+    window.location.reload();
   }
 
   editPassword() {
     if  (this.infoForm.get('password').enabled) {
       this.infoForm.get('password').disable();
+      this.infoForm.get('confirmPassword').disable();
       this.editingPassword = false;
       this.passwordLabel = 'Mot de passe';
       this.infoForm.get('password').setValue('****************');
     } else {
       this.infoForm.get('password').enable();
+      this.infoForm.get('confirmPassword').enable();
       this.infoForm.get('password').setValue('');
+      this.infoForm.get('confirmPassword').setValue('');
       this.editingPassword = true;
       this.passwordLabel = 'Saisir le nouveau mot de passe';
     }
